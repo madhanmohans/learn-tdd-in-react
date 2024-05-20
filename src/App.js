12c4ed6523d4c0b00d4b03b71be46b815e561f18
madhanmohans <sherlockholmes.missme@gmail.com>
2024-05-21 00:08:16 +0530

    added message listing functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import NewMessageForm from './NewMessageForm';
+import MessageList from './MessageList';
 
 export default function App() {
-  function handleSend() {}
-  return <NewMessageForm onSend={ handleSend }/>;
+  const [messages, setMessages] = useState([]);
+  function handleSend(newMessage) {
+    setMessages([newMessage, ...messages]);
+  }
+  return (
+    <>
+      <NewMessageForm onSend={handleSend}/>
+      <MessageList data={messages} />
+    </>
+  )
 }
---
+++ src/MessageList.js
@@ -0,0 +1,14 @@
+export default function MessageList({data}) {
+    return(
+        <ul>
+            {
+                data.map(
+                    message => 
+                    <li key = {message}>
+                    {message}
+                    </li>
+                )
+            }
+        </ul>
+    );
+}
